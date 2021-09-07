c2bd747e0f702c2b2aafbeca4cbad02abc809e84
ademarsoad <ademarsoad@gmail.com>
2021-09-07 02:56:19 -0300

    Basic static components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,8 +1,22 @@
+import DataTable from "compoents/DataTable";
+import Footer from "compoents/Footer";
+import NavBar from "compoents/NavBar";
+
 function App() {
   return (
-    <div>
-      <h1 className="text-primary">Olá mundo!</h1>
-    </div>
+    <><NavBar />
+      <div className="container">
+
+        <h1 className="text-primary">Olá mundo!</h1>
+
+        <DataTable />
+
+      </div>
+
+      
+
+      <Footer />
+    </>
   );
 }
 
