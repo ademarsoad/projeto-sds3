36aa3af18e54c6fb1663045b200c9f302ee71c68
ademarsoad <ademarsoad@gmail.com>
2021-09-09 22:07:58 -0300

    Pagination

---
+++ backend/src/main/java/com/devsuperir/dsvendas/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperir.dsvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperir.dsvendas.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+	
+}
